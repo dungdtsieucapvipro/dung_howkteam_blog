163af46cf71af500206d39e2bd043853ab0562b3
dungdtsieucapvipro <149314686+dungdtsieucapvipro@users.noreply.github.com>
2024-09-09 11:16:05 +0700

    Chap 26

--- WebLandingPage/resources/js/scripts.js
+++ WebLandingPage/resources/js/scripts.js
@@ -20,5 +20,20 @@ $(document).ready(
             }, 700);
             event.preventDefault();
         })
+        // mobile navigation
+        $('.mobile-nav-icon').click(
+            function(){
+                $('.main-nav').slideToggle(200);
+
+                if($('.mobile-nav-icon').hasClass('fa-bars')){
+                    $('.mobile-nav-icon').addClass('fa-times');
+                    $('.mobile-nav-icon').removeClass('fa-bars')
+                }
+                else{
+                    $('.mobile-nav-icon').addClass('fa-bars');
+                    $('.mobile-nav-icon').removeClass('fa-times')
+                }
+            }
+        )
     }
 )
